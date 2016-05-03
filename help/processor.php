f60ea28a856b2ed28a276c7dce51c98b6a5fef51
Pablo Fernandez <contact@pablofernandez.com>
2016-05-03 14:11:58 -0700

    Update processor.php

--- help/processor.php
+++ help/processor.php
@@ -1,3 +1,7 @@
 <?php
+// Helper Script for the Cloud Analyze API connection. 
+// OpenALPR hosts an open API connection that can be curled. 
+// If people would like to not rely on a 3rd party API, the C++ files are also in this GitHub sketch. 
+// 
 $ch = curl_init('https://api.openalpr.com/v1/recognize');
 ?>
